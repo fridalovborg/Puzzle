f6878bb8d6358364848596c4e972e649969ea540
Frida Lövborg <frida.lovborg@gmail.com>
2020-08-26 17:58:53 +0200

    init App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './css/App.css';
+import Puzzle from './components/Puzzle';
+
+function App() {
+  return (
+    <Puzzle></Puzzle>
+  );
+}
+
+export default App;
